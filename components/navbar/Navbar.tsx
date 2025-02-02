f717b44c79fc32c5edf9cea5636bc9e3160091f2
LalithChowdary <v.lalithchowdary@gmail.com>
2025-02-02 14:18:27 +0530

    nav bar and footer

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center py-4">
+        <Link href="/">
+          <img className="w-[8vw]" src="/icon.jpeg" alt="health sphere" />
+        </Link>
+        <div className="flex items-center space-x-6">
+          <div>Home</div>
+          <div>Book Appointment</div>
+          <div>Pill Box</div>
+          <div>Dasboard</div>
+          <div>Profile</div>
+          <div className="flex gap-2">
+            <Button asChild variant="outline" className="border-black">
+              <Link href="/login">Login</Link>
+            </Button>
+            <Button asChild className="bg-black text-white">
+              <Link href="/login">Sign Up</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
